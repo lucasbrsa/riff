e2b6a16835ed5f1be425d01af3db65cb603c1a6d
Ruchir Bapat <ruchir.bapat@gmail.com>
2017-08-11 18:38:45 +1000

    Added a hashmap/dictionary

--- riff/hashmap.c
+++ riff/hashmap.c
@@ -1,202 +1,99 @@
-/*
-#include "hashmap.h"
-#include <stdio.h>
-#include <malloc.h>
-#include <string.h>
-
-bucket* search_for_key(bucket* root, void* key_to_find);
-unsigned int get_hashcode(void* key);
-int power(int base, int exponent);
-unsigned int get_index(unsigned int hashcode);
-bucket* create(hashmap_pair data, bucket* next);
-bool add_to_start(bucket* start, hashmap_pair data);
-bucket* search_for_key(bucket* root, void* key_to_find);
-
-bool set(hashmap* map, void* key, void* value)
-{
-	hashmap_pair data = { key, value };
-	unsigned hashcode = get_hashcode(key);
-	unsigned index = get_index(hashcode);
-	printf("Hashcode for %s is %i and the index will be %i\n", (char*)key, hashcode, index);
-	
-	return add_to_start(&(map->buckets[get_index(get_hashcode(key))]), data);
-}
-
-void* get(hashmap* map, void* key)
-{
-	return search_for_key(&(map->buckets[get_index(get_hashcode(key))]), key)->data.value;
-}
-
-unsigned int get_hashcode(void* key)
-{
-	unsigned int hashcode = 0;
-	const char* str = (char*)key;
-	for (int i = 0, len = strlen(str); i < len; i++) { hashcode += (int)str[i] * power(31, len - (i + 1)); }
-	return hashcode;
-}
-
-int power(int base, int exponent)
-{
-	if (!exponent)
-		return 1;
-	int result = 1;
-	for (int i = 0; i < exponent; i++)
-		result *= base;
-	return result;
-}
-
-unsigned int get_index(unsigned int hashcode)
-{
-	return hashcode % MAX_MAP_SIZE;
-}
-
-bucket* create(hashmap_pair data, bucket* next)
-{
-	bucket* b = malloc(sizeof(bucket));
-	memcpy(&b->data, &data, sizeof(data));
-	b->next = next;
-	return b;
-}
-
-
-bool add_to_start(bucket* start, hashmap_pair data)
-{
-	bucket* to_add = create(data, start);
-	start = to_add;
-	return true;
-}
-
-bucket* search_for_key(bucket* root, void* key_to_find)
-{
-	bucket* cursor = root;
-	while (cursor) {
-		if (cursor->data.key == key_to_find)
-			return cursor;
-		else cursor = cursor->next;
-	}
-	return 0;
-}
-*/
-
 #if 1
 #include "hashmap.h"
-#include <stdio.h>
-#include <malloc.h>
-#include <string.h>
 
-bucket create(hashmap_pair data, bucket* next);
-int get_hashcode(void* key);
+#include <malloc.h>
+
+hashmap_bucket bucket_create(char* key, void* value, hashmap_bucket* next);
+unsigned int get_hashcode(char* key);
 unsigned int get_index(int hashcode);
 int power(int base, int exponent);
-bool insert(bucket* list, hashmap_pair data);
-void* search(hashmap* map, void* key);
+_Bool insert(hashmap_bucket* list, char* key, void* value);
+void* search(hashmap* map, char* key);
 
-unsigned int get_hashcode(void* key)
-{
+unsigned int get_hashcode(char* key) {
 	unsigned int hashcode = 0;
 	const char* str = (char*)key;
-	for (int i = 0, len = strlen(str); i < len; i++) { hashcode += (int)str[i] * power(31, len - (i + 1)); }
+	
+	for (int i = 0, len = strlen(str); i < len; i++) { 
+		hashcode += (int)str[i] * power(31, len - (i + 1)); 
+	}
+	
 	return hashcode;
 }
 
-unsigned int get_index(int hashcode)
-{
-	return +(hashcode % MAX_MAP_SIZE);
+unsigned int get_index(unsigned hashcode, size_t s) {
+	return +(hashcode % s);
 }
 
-hashmap hashmap_create()
-{
+hashmap hashmap_create(size_t size) {
 	hashmap h = *(hashmap*)malloc(sizeof(hashmap));
-	hashmap_pair tmp = hashmap_pair_create(0, 0);
-	for (int i = 0; i < MAX_MAP_SIZE; i++)
-	{
-		h.buckets[i] = create(tmp, 0);
-	}
+	for (int i = 0; i < size; i++)
+		h.buckets[i] = bucket_create(tmp, 0);
+
 	return h;
 }
 
-void hashmap_free(hashmap* map)
-{
-	for (int i = 0; i < MAX_MAP_SIZE; i++) { free((void*)&map->buckets[i]); }
+void hashmap_free(hashmap* map) {
+	for (int i = 0; i < map->size; i++) 
+		free((void*)&map->buckets[i]);
+
 	free(map);
 }
 
-hashmap_pair hashmap_pair_create(void* key, void* value)
-{
-	hashmap_pair p = *(hashmap_pair*)malloc(sizeof(key) * sizeof(value));
-	p.key = key; p.value = value;
-	return p;
+_Bool hashmap_set(hashmap* map, char* key, void* value) {
+	map->buckets[get_index(get_hashcode(key), map->size)].value = "meme";
+	return insert(&map->buckets[get_index(get_hashcode(key), map->size)], key, value);
 }
 
-void hashmap_pair_free(hashmap_pair* pair)
-{
-	free(pair->key);
-	free(pair->value);
-	free(pair);
+void* hashmap_get(hashmap* map, char* key) {
+	return search(map, key);
 }
 
-bool set(hashmap* map, hashmap_pair data)
-{
-	printf("%s", map->buckets[0].data.key);
-	return insert(&map->buckets[get_index(get_hashcode(data.key))], data);
-}
+int power(int base, int exponent) {
+	if (!exponent) return 1;
 
-void* get(hashmap* map, void* key)
-{
-	return search(&map->buckets[get_index(get_hashcode(key))], key);
-}
-
-int power(int base, int exponent)
-{
-	if (!exponent)
-		return 1;
 	int result = 1;
 	for (int i = 0; i < exponent; i++)
 		result *= base;
+	
 	return result;
 }
 
-bucket create(hashmap_pair data, bucket* next)
-{
-	bucket* b = malloc(sizeof(bucket));
-	b->data = hashmap_pair_create(data.key, data.value);
+hashmap_bucket bucket_create(char* key, void* value, hashmap_bucket* next) {
+	hashmap_bucket* b = malloc(sizeof(hashmap_bucket));
+	//b->data = hashmap_pair_create(data.key, data.value);
 	b->next = next;
 	return *b;
 }
 
-bool insert(bucket* list, hashmap_pair data)
-{
-	/*list->next = malloc(sizeof(bucket*));
-	*(list->next) = create(data, 0);
-	bucket* tmp = list->next;
-	*(list->next) = create(data, tmp);
-	return true;*/
-	bucket * current = list;
-	while (current->next != NULL) {
+_Bool insert(hashmap_bucket* list, char* key, void* value) {
+		
+	hashmap_bucket* current = list;
+	
+	while (current->next != NULL) 
 		current = current->next;
-	}
-
-	/* now we can add a new variable */
-	//current = malloc(sizeof(bucket));
-	current->data = data;
-	//current->next->next = NULL;
+	current->key = key;
+	current->value = value;
 	current->next = NULL;
-
-	return true;
+	
+	return 1;
 }
 
-void* search(hashmap* map, void* key)
-{
-	bucket* cursor = malloc(sizeof(bucket*));
-	memcpy(cursor, (void*)&map->buckets[get_index(get_hashcode(key))], sizeof(bucket));
-	//map->buckets[get_index(get_hashcode(key))];
-	while(cursor->next != 0)
-	{
-		//if (memcmp((void*)&cursor->data, (void*)&pair_to_find, sizeof(pair_to_find))) { return cursor; }
-		if (memcmp(cursor->data.key, key, sizeof(key))) { return cursor->data.key; }
-		else { cursor = cursor->next; }
-	}
+void* search(hashmap* map, char* key) {
+	hashmap_bucket* current = &(map->buckets[get_index(get_hashcode(key), map->size)]);
+
+	do {
+		if (current->key == key)
+			return current->value;
+		current = current->next;
+	} while (current->next != NULL);
+
 	return 0;
+	//return (*map).buckets[get_index(get_hashcode(key))].data.value;
 }
 
+void* hashmap_resize(hashmap* map, size_t new_size)
+{
+	return realloc(map, sizeof(map) * new_size);
+}
 #endif
--- riff/hashmap.h
+++ riff/hashmap.h
@@ -1,68 +1,28 @@
-/*
-#ifndef _HASHMAP_H
-#define _HASHMAP_H
-
-#include <stdbool.h>
-
-#define MAX_MAP_SIZE 10
-
-typedef struct hashmap_pair_t
-{
-	void* key;
-	void* value;
-} hashmap_pair;
-
-typedef struct bucket_t
-{
-	hashmap_pair data;
-	struct bucket_t* next;
-} bucket;
-
-typedef struct hashmap_t
-{
-	bucket buckets[MAX_MAP_SIZE];
-} hashmap;
-
-bool set(hashmap* map, void* key, void* value);
-void* get(hashmap* map, void* key);
-unsigned int get_hashcode(void* key);
-#endif
-*/
-
 #if 1
 #ifndef _HASHMAP_H
 #define _HASHMAP_H
 
-#include <stdbool.h>
+#include <string.h>
 
-#define MAX_MAP_SIZE 10
-
-//alloc hashmap_pair given key and value size in bytes (sizeof())
-typedef struct hashmap_pair_t
-{
-	void* key;
+/* hashmap linked-list bucket type */
+typedef struct hashmap_bucket_t {
+	char* key;
 	void* value;
-} hashmap_pair;
+	struct hashmap_bucket_t* next;
+} hashmap_bucket;
 
-typedef struct bucket_t
-{
-	hashmap_pair data; // need to alloc this data before use
-	struct bucket_t* next;
-} bucket;
-
-typedef struct hashmap_t
-{
-	bucket buckets[MAX_MAP_SIZE];
+typedef struct hashmap_t {
+	hashmap_bucket* buckets;
+	size_t size;
 } hashmap;
 
-hashmap hashmap_create();
+hashmap hashmap_create(size_t size);
 void hashmap_free(hashmap* map);
 
-hashmap_pair hashmap_pair_create(void* key, void* value);
-void hashmap_pair_free(hashmap_pair* pair);
+_Bool hashmap_set(hashmap* map, char* key, void* data);
+void* hashmap_get(hashmap* map, char* key);
 
-bool set(hashmap* map, hashmap_pair data); 
-void* get(hashmap* map, void* key);
+void* hashmap_resize(hashmap* map);
 
 #endif
 #endif
