db79951340578c100f5fb20adf3c05a933623499
Ruchir Bapat <18492574+ruchirbapat@users.noreply.github.com>
2017-09-10 02:59:25 +1000

    Update hashmap.c

--- riff/hashmap.c
+++ riff/hashmap.c
@@ -1,76 +1,131 @@
+/* includes */
 #include "hashmap.h"
+
 #include <malloc.h>
 
+/* hash a key (string of characters) using the hashmap size */
+unsigned int get_hashcode(char* key);
+
+/* get a bucket index from a hashed key, given the hashmap size */
+unsigned int get_index(unsigned hashcode, size_t s);
+
+/* raise a number to a power, useful for the hashing algorithm implemented */
+int power(int base, int exponent);
+
+/* insert data into the hashmap */
+_Bool insert(hashmap_bucket* list, char* key, void* value);
+
+/* search for a key-value pair in the hashmap */
+void* search(hashmap* map, char* key);
+
+/* hashes a key */
 unsigned int get_hashcode(char* key) {
+	
+	/* shitty implementation */
 	unsigned int hashcode = 0;
+
+	/* why */
 	const char* str = (char*)key;
-	
-	for (int i = 0, len = strlen(str); i < len; i++) { 
-		hashcode += (int)str[i] * power(31, len - (i + 1)); 
+
+	/* uses the Java hashing algorithm */
+	for (int i = 0, len = strlen(str); i < len; i++) {
+		hashcode += (int)str[i] * power(31, len - (i + 1));
 	}
-	
+
 	return hashcode;
 }
 
+/* get the bucket index from the hashmap */
 unsigned int get_index(unsigned hashcode, size_t s) {
-	return +(hashcode % s);
+	return +(hashcode % s); /* std::abs is too kool for me */
 }
 
+/* create a hashmap, anon's favourite function! */
 hashmap hashmap_create(size_t size) {
+	
 	hashmap h;
-	h.buckets = calloc(sizeof(bucket), size);
+
+	/* callocate memory for the buckets */ /* i'm funny */
+	h.buckets = calloc(sizeof(hashmap_bucket), size);
+	
+	/* set size */
 	h.size = size;
+	
+	/* ret */
 	return h;
 }
 
+/* free the mem used by the lame hash map */
 void hashmap_free(hashmap* map) {
-	for (int i = 0; i < map->size; i++) 
+
+	/* iterate and call free() on all the memes */
+	for (int i = 0; i < map->size; i++)
 		free((void*)&map->buckets[i]);
 
 	free(map);
 }
 
+/* set a value in the hashmap */
 _Bool hashmap_set(hashmap* map, char* key, void* value) {
-	return insert(&map->buckets[get_index(get_hashcode(key), map->size)], key, value);
+	
+	/* calculate the bucket we'll be inserting into */
+	hashmap_bucket* hb = &map->buckets[get_index(get_hashcode(key), map->size)];
+	
+	/* insert */
+	return insert(hb, key, value);
 }
 
+/* get a value associated to a key from a hashmap */
 void* hashmap_get(hashmap* map, char* key) {
+	/* call the linked-list searcher */
 	return search(map, key);
 }
 
+/* raise a number, lame meme */
 int power(int base, int exponent) {
+	
+	/* so we don't get caught out!1!! */
 	if (!exponent) return 1;
 
+	/* yeah yeah iterate and be cool */
 	int result = 1;
 	for (int i = 0; i < exponent; i++)
 		result *= base;
-	
+
 	return result;
 }
 
-hashmap_bucket bucket_create(char* key, void* value, hashmap_bucket* next) {
-	hashmap_bucket hb;
-	hb.key = key;
-	hb.value = value;
-	hb.next = next;
-	return b;
-}
-
+/* insert into a linked list */
 _Bool insert(hashmap_bucket* head, char* key, void* value) {
 
-	hashmap_bucket hb;
-	hb.next = head.next;
-	head.next = &hb;
-	hb.key = key;
-	hb.value = value;
+	/* check if the linked list is not sized 1 */
+	if (head->next != NULL)
+	{
+		/* bucket to add */
+		hashmap_bucket* hb = malloc(sizeof(hashmap_bucket));
+		
+		/* set properties */
+		hb->next = head->next;
+		hb->key = key;
+		hb->value = value;
+		head->next = hb;
 
+	} else {
+		/* inserting into the first element */
+		head->key = key;
+		head->value = value;
+	}
+	
 	return 1;
 }
 
+/* search in a linked list */
 void* search(hashmap* map, char* key) {
+	/* calculate which bucket is of interest */
 	hashmap_bucket* current = &(map->buckets[get_index(get_hashcode(key), map->size)]);
 
-	do {
+	/* step through, checking if the key matches and return if it does */
+	do { /* at least once */
 		if (current->key == key)
 			return current->value;
 		current = current->next;
@@ -79,7 +134,9 @@ void* search(hashmap* map, char* key) {
 	return NULL;
 }
 
+/* resize the hashmap */
 void* hashmap_resize(hashmap* map, size_t new_size)
 {
+	/* reallocate memory */
 	return realloc(map, sizeof(map) * new_size);
 }
