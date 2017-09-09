f3188e975a8ee0f3f1649d6d797e9915da7dbc31
Ruchir Bapat <18492574+ruchirbapat@users.noreply.github.com>
2017-09-10 02:59:15 +1000

    Update hashmap.h

--- riff/hashmap.h
+++ riff/hashmap.h
@@ -1,33 +1,47 @@
 #ifndef _HASHMAP_H
 #define _HASHMAP_H
 
+/* includes */
 #include <string.h>
 
+/* hashmap bucket linked-list type */
 typedef struct hashmap_bucket_t {
+	
+	/* hashable key */
 	char* key;
-	void* value;
-	struct hashmap_bucket_t* next;
+	
+	/* stored value */
+	void* value; 
+	
+	/* points to next element */
+	struct hashmap_bucket_t* next; 
+
 } hashmap_bucket;
 
+/* hashmap type struct */
 typedef struct hashmap_t {
+	
+	/*holds a variably sized list of buckets */
 	hashmap_bucket* buckets;
+
+	/* number of buckets */
 	size_t size;
+
 } hashmap;
 
-hashmap_bucket bucket_create(char* key, void* value, hashmap_bucket* next);
-unsigned int get_hashcode(char* key, size_t s);
-unsigned int get_index(int hashcode);
-int power(int base, int exponent);
-_Bool insert(hashmap_bucket* list, char* key, void* value);
-void* search(hashmap* map, char* key);
-
-
+/* create and initialise a hashmap with some size */
 hashmap hashmap_create(size_t size);
+
+/* free all the memory used by a hashmap */
 void hashmap_free(hashmap* map);
 
+/* insert data into the hashmap with a hashable key */
 _Bool hashmap_set(hashmap* map, char* key, void* data);
+
+/* retrieve data from the hashmap given a look-up key */
 void* hashmap_get(hashmap* map, char* key);
 
-void* hashmap_resize(hashmap* map);
+/* resize the number of buckets to a given new size */
+void* hashmap_resize(hashmap* map, size_t new_size);
 
 #endif
